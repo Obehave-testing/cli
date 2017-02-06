7c276a8c32b0a349431603218b1add741b45ef7b
owen Bodley <owen@biomatters.com>
2017-02-06 18:38:39 +1300

    master - start cli project

---
+++ helpers.js
@@ -0,0 +1,12 @@
+
+function tryParseInt(arg) {
+    const result = parseInt(arg);
+    // Parse int returns NaN if the first character in `arg` cannot be converted to a string.
+    // @see https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/parseInt
+    if (isNaN(result)) {
+        throw Error("Argument port must be an integer. '" + arg + "' is not valid.");
+    }
+    return result;
+}
+
+module.exports = {tryParseInt}
